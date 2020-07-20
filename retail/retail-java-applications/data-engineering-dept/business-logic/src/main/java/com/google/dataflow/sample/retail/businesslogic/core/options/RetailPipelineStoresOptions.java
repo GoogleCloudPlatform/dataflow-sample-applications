4e0671e4d51c7c9faee2af1b2992edee95c95d98
rarokni <rezarokni@google.com>
2020-07-20 22:44:02 +0800

    fixup

--- retail/retail-java-applications/data-engineering-dept/business-logic/src/main/java/com/google/dataflow/sample/retail/businesslogic/core/options/RetailPipelineStoresOptions.java
+++ retail/retail-java-applications/data-engineering-dept/business-logic/src/main/java/com/google/dataflow/sample/retail/businesslogic/core/options/RetailPipelineStoresOptions.java
@@ -17,11 +17,8 @@
  */
 package com.google.dataflow.sample.retail.businesslogic.core.options;
 
-
 import org.apache.beam.sdk.annotations.Experimental;
-
 import org.apache.beam.sdk.options.Default;
-
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 
