0c914cac08a01e5e4aef6ba315ab1069dc639385
rarokni <rezarokni@google.com>
2020-11-25 07:54:00 +0800

    [Timeseries] Fixup missing files from commit

---
+++ timeseries-streaming/timeseries-python-applications/setup.py
@@ -0,0 +1,26 @@
+# Lint as: python2, python3
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+setuptools.setup(
+    name='tsflow',
+    version='0.3.2-sample',
+    python_requires='>=3.7',
+    install_requires=[
+        'tfx==0.24.0', 'kfp==1.0.3'
+    ],
+    packages=setuptools.find_packages()
+)
