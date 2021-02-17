409560852c4de76d4f0c263e77e2084f6f15c3d3
Wei Xiaoliang <xiaoliangntu@gmail.com>
2021-02-17 04:33:51 +0000

    Add terraform scripts for pubsub and enable related APIs
    Rename terraform folder
    
    Parameterise region and edit README file for -auto-approve flag, link for cloud shell and link for available regions

---
+++ retail/retail-java-applications/terraform/variables.tf
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  type        = string
+  description = "Project ID in GCP"
+}
+
+variable "region" {
+  type    = string
+}
+
+variable "topic_clickstream_inbound" {
+  type    = string
+}
+
+variable "topic_transactions_inbound" {
+  type    = string
+}
+
+variable "topic_inventory_inbound" {
+  type    = string
+}
+
+variable "topic_inventory_outbound" {
+  type    = string
+}
+
+variable "clickstream_inbound_sub" {
+  type    = string
+}
+
+variable "transactions_inbound_sub" {
+  type    = string
+}
+
+variable "inventory_inbound_sub" {
+  type    = string
+}
+
